8e8e44d8fd719660deaa55be3e5550f2291d65d0
ashwin <ashwinss3@gmail.com>
2022-11-17 02:58:11 +0530

    Minor changes

--- bingo/utils/utils.py
+++ bingo/utils/utils.py
@@ -1,6 +1,6 @@
-from datetime import datetime
+from datetime import datetime, timezone
 
 
 def get_active_games():
     from bingo.models import Game
-    return Game.objects.filter(end_time__gt=datetime.utcnow())
+    return Game.objects.filter(end_time__gt=datetime.now(timezone.utc))
