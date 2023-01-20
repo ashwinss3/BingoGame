03b16ded8b44483f9572a6ee743fa5a597b28959
ashwin <ashwinss3@gmail.com>
2023-01-21 00:29:55 +0530

    Added game filter Game options in Admin dashboard.

--- bingo/admin.py
+++ bingo/admin.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User, Game, GameOptions, League, UserGame, UserGameChoices, LeagueStandings
 
+class PerGameOptions(admin.ModelAdmin):
+    list_filter = ('game',)
+
 admin.site.register(User, UserAdmin)
 admin.site.register(Game)
-admin.site.register(GameOptions)
+admin.site.register(GameOptions, PerGameOptions)
 admin.site.register(League)
 admin.site.register(LeagueStandings)
 admin.site.register(UserGame)
