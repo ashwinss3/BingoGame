97f584e45b862561b6cf042173ff112b171139e9
ashwin <ashwinss3@gmail.com>
2023-01-23 00:52:28 +0530

    Added is_done to display in Admin dashboard.

--- bingo/admin.py
+++ bingo/admin.py
@@ -3,6 +3,7 @@ from django.contrib.auth.admin import UserAdmin
 from .models import User, Game, GameOptions, League, UserGame, UserGameChoices, LeagueStandings
 
 class PerGameOptions(admin.ModelAdmin):
+    list_display = ('name', 'is_done')
     list_filter = ('game',)
 
 admin.site.register(User, UserAdmin)
